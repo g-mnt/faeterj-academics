76902dfa538f10350a64962be311ca049be122f8
Gabriel Monteiro <gmonteiro.arg@gmail.com>
2024-06-13 19:46:22 -0300

    remove mocks para deixar os testes mais proximos da realidade

--- __mocks__/setup.ts
+++ __mocks__/setup.ts
@@ -1,6 +1,5 @@
 import "./navigationMock";
 import "./authRepositoryMock";
-import "./userStoreMock";
 
 jest.mock('@react-native-async-storage/async-storage', () =>
     require('@react-native-async-storage/async-storage/jest/async-storage-mock')
