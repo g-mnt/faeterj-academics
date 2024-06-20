12c17bf1f66430bbd402277ba596984f61c1517d
Gabriel Monteiro <gmonteiro.arg@gmail.com>
2024-06-20 19:20:48 -0300

    adicionado loadings e empty message

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,38 +1,41 @@
-import {View, StyleSheet, FlatList} from "react-native";
-import {withAuthLayout} from "src/HOC/withAuthLayout";
-import {ArticleCard} from "components/ArticleCard";
-import {ArticleRepository} from "repositories/article";
-import {Text} from "react-native-paper";
-import {useInfinityScroll} from "hooks/useInfinityScroll";
+import React from 'react'
+import { View, StyleSheet, FlatList } from 'react-native'
+import { withAuthLayout } from 'src/HOC/withAuthLayout'
+import { ArticleCard } from 'components/ArticleCard'
+import { ArticleRepository } from 'repositories/article'
+import { useInfinityScroll } from 'hooks/useInfinityScroll'
+import { NoDataCard } from 'src/components/NoDataCard'
+import { ActivityIndicator } from 'react-native-paper'
 
 export const HomeScreen = withAuthLayout(() => {
-    const {data, loadMore} = useInfinityScroll(ArticleRepository.index, {params: {}});
-    return (
+  const { data, isLoading, loadMore } = useInfinityScroll(ArticleRepository.index, { params: {} })
+  return (
         <View>
-            {data ? (
-                <FlatList
-                    data={data}
-                    onEndReached={loadMore}
-                    renderItem={({item, index})  => (
-                        <ArticleCard
-                            key={index}
-                            style={styles.article}
-                            article={{
-                                ...item,
-                                favorite: false
-                            }}
-                        />
-                    )}
-                />
-            ) : <Text>No data</Text>}
-
+            <FlatList
+                data={data}
+                onEndReached={() => { loadMore().catch(() => {}) }}
+                renderItem={({ item, index }) => (
+                    <ArticleCard
+                        key={index}
+                        style={styles.article}
+                        article={{
+                          ...item,
+                          favorite: false
+                        }}
+                    />
+                )}
+                ListEmptyComponent={
+                    !isLoading ? <NoDataCard message={'Nenhum artigo encontrado'} /> : null
+                }
+                ListFooterComponent={isLoading ? <ActivityIndicator /> : null}
+            />
         </View>
-    )
-});
+  )
+})
 
 const styles = StyleSheet.create({
-    article: {
-        marginBottom: 20,
-        marginHorizontal: 2,
-    }
-});
+  article: {
+    marginBottom: 20,
+    marginHorizontal: 2
+  }
+})
